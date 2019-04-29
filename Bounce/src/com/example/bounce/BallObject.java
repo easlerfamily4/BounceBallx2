09395cfcbcacf66ee1f780a11ed866f0e11d4846
Meghan <meghanemiles@student.midlandstech.edu>
2019-04-29 12:19:09 -0400

    Initial commit

---
+++ Bounce/src/com/example/bounce/BallObject.java
@@ -0,0 +1,186 @@
+package com.example.bounce;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class BallObject {
+
+	private double height;
+	private double width;
+	private double posx;
+	private double posy;
+	private double accx;
+	private double accy;
+	private double friction = 0.025;
+	private BlocksObject block = null;
+
+	private SoundPool sounds;
+	private int bounceSound;
+	private int touchSound;
+
+	private double bounce;
+	private double defbounce;
+
+	AudioManager audio;
+	float currentVolume;
+
+	private int radius;
+	private Paint p;
+
+	public BallObject(double posx, double posy, double accx, double accy,
+			double bounce, int radius, Paint p, View v) {
+		this.posx = posx;
+		this.posy = posy;
+		this.accx = accx;
+		this.accy = accy;
+		this.bounce = bounce;
+		this.defbounce = bounce;
+		this.radius = radius;
+		this.p = p;
+
+		sounds = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+		bounceSound = sounds.load(v.getContext(), R.raw.bounce, 1);
+		touchSound = sounds.load(v.getContext(), R.raw.piu, 1);
+
+		audio = (AudioManager) v.getContext().getSystemService(
+				Context.AUDIO_SERVICE);
+	}
+
+	public double getPosx() {
+		return posx;
+	}
+
+	public double getPosy() {
+		return posy;
+	}
+
+	public double getAccx() {
+		return accx;
+	}
+
+	public double getAccy() {
+		return accy;
+	}
+
+	public double getBounce() {
+		return bounce;
+	}
+
+	public int getRadius() {
+		return radius;
+	}
+
+	public Paint getP() {
+		return p;
+	}
+
+	public void setBlock(BlocksObject block) {
+		this.block = block;
+	}
+
+	public void setPosx(double posx) {
+		this.posx = posx;
+	}
+
+	public void setPosy(double posy) {
+		this.posy = posy;
+	}
+
+	public void setAccx(double accx) {
+		this.accx = accx;
+	}
+
+	public void setAccy(double accy) {
+		this.accy = accy;
+	}
+
+	public void setBounce(double bounce) {
+		this.bounce = bounce;
+	}
+
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+
+	public void setP(Paint p) {
+		this.p = p;
+	}
+
+	public void update() {
+
+		posy -= accy;
+		accy -= 0.5;
+		posx += accx;
+
+		if (block.isBlockDeleted() == false) {
+			if (block.getRect().contains((int) posx, (int) posy + radius)
+					|| block.getRect()
+							.contains((int) posx, (int) posy - radius)) {
+				playsound();
+				block.delete();
+				accy = accy * -1;
+			}
+			if (block.getRect().contains((int) posx + radius, (int) posy)
+					|| block.getRect()
+							.contains((int) posx - radius, (int) posy)) {
+				playsound();
+				block.delete();
+				accx = accx * -1;
+			}
+		}
+
+		if (accx > 0)
+			accx -= friction;
+		if (accx < 0)
+			accx += friction;
+
+		if (posy >= (height - radius)) {
+			playsound();
+			accy = (Math.abs(accy) * bounce);
+			if (bounce > 0)
+				bounce -= 0.01;
+		}
+
+		if (posy <= (0 + radius)) {
+			playsound();
+			accy = 0 - Math.abs(accy) * bounce;
+		}
+
+		if (posx >= (width - radius)) {
+			playsound();
+			accx = 0 - Math.abs(accx) * bounce;
+		}
+
+		if (posx <= (0 + radius)) {
+			playsound();
+			accx = Math.abs(accx) * bounce;
+		}
+	}
+
+	public void playsound() {
+		currentVolume = audio.getStreamVolume(AudioManager.STREAM_MUSIC) / 15.0f;
+		sounds.play(bounceSound, (float) Math.abs(bounce * bounce)
+				* currentVolume, (float) Math.abs(bounce * bounce)
+				* currentVolume, 0, 0, 1.0f);
+	}
+
+	public void draw(Canvas c) {
+
+		height = c.getClipBounds().height();
+		width = c.getClipBounds().width();
+		c.drawCircle((float) posx, (float) posy, (float) radius, p);
+	}
+
+	public void control(MotionEvent e) {
+		currentVolume = audio.getStreamVolume(AudioManager.STREAM_MUSIC) / 15.0f;
+		sounds.play(touchSound, currentVolume, currentVolume, 0, 0, 1.5f);
+		accx = (e.getX() - posx) / 25;
+		accy = (e.getY() - posy) / -25;
+		bounce = defbounce;
+	}
+}
---
+++ Bounce/src/com/example/bounce/BallView.java
@@ -0,0 +1,54 @@
+package com.example.bounce;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.View;
+
+@SuppressLint({ "DrawAllocation", "ParserError" })
+public class BallView extends View {
+
+	private Paint mPaint;
+	private BallObject ball;
+	public BlocksObject block;
+
+	public BallView(Context context) {
+		super(context);
+		mPaint = new Paint();
+		mPaint.setARGB(0xFF, 0x00, 0x80, 0xFF);
+		ball = new BallObject(100.0, 50.0, 4.0, 10.0, 0.8, 10, mPaint, this);
+		block = new BlocksObject(400, 1000, 450, 1050);
+		ball.setBlock(block);
+	}
+
+	private void update() {
+
+		ball.update();
+		try {
+			Thread.sleep(1);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		this.invalidate();
+	}
+
+	@Override
+	public void onDraw(Canvas canvas) {
+
+		ball.draw(canvas);
+		block.draw(canvas);
+		this.update();
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+
+		ball.control(event);
+
+		return super.onTouchEvent(event);
+	}
+
+}
